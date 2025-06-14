f339409f3750e293b7ea401ef884cbab17801195
zkaiLGK <hanphong1530@gmail.com>
2025-06-14 21:06:04 +0700

    chore(security): allow Vercel frontend to access API (CORS config)

--- src/main/java/com/datn/event_manager/configuration/SecurityConfig.java
+++ src/main/java/com/datn/event_manager/configuration/SecurityConfig.java
@@ -65,7 +65,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // đúng port của React
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://hi-eventify.netlify.app",
+                "https://hi-eventify.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // quan trọng nếu dùng token hoặc cookie
